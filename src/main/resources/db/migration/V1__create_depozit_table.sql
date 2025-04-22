5e3191525faf080246f7502d22bcfae39252ccb3
Andrei Ureche <ureche96@gmail.com>
2025-04-23 00:50:06 +0300

    ...

--- src/main/resources/db/migration/V1__create_depozit_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       nume VARCHAR(100) NOT NULL,
-                       parola VARCHAR(255) NOT NULL,
-                       privilegiu VARCHAR(20) NOT NULL,
-                       depozit_id INT
-);
